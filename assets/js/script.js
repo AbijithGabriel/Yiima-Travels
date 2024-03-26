453d71bb9ce59422fdf67f7bd0171947471d2ce0
karthik <karthikr0591@gmail.com>
2024-03-26 20:12:29 +0530

    Add - reveal on scroll, hover effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ navToggleBtn.addEventListener("click", function () {
 
 
 /**
- * show go top btn when scroll window to 500px
+  show go top btn when scroll window to 500px
  */
 
 const goTopBtn = document.querySelector("[data-go-top]");
@@ -24,3 +24,28 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 500 ? goTopBtn.classList.add("active")
     : goTopBtn.classList.remove("active");
 });
+
+
+/*-----------------------------------*\
+  REVEAL ON SCROLL
+\*-----------------------------------*/
+
+window.addEventListener('scroll', reveal)
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i=0; i < reveals.length; i++){
+
+    var windowHieght = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if(revealTop < windowHieght - revealPoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
